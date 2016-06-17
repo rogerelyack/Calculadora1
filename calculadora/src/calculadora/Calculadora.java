4c1e7f8ca50af85a97673b5fe28480e1d8aaa13f
roger <rogeryauri@gmail.com>
2016-06-17 17:58:56 +0200

    operaiones terminadas

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -23,4 +23,10 @@ public class Calculadora {
      float restar(float a,float b){
     return a-b;
     }
+      float multiplicacion(float a,float b){
+    return a*b;
+    }
+       float division(float a,float b){
+    return a/b;
+    }
 }
